5aaf144701a5878f35ba9f21897dcb1e5990af47
s-shemmee <shemmee.dev@gmail.com>
2025-04-11 20:19:14 +0100

    feat: add ESLint and Prettier configuration files, update package dependencies, and format code style

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['react', '@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-console': 'warn',
+    'no-unused-vars': 'off',
+    'react/no-unescaped-entities': 'off',
+  },
+};
